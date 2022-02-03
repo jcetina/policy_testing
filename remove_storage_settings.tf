7f0a2bb75d76481c1ac8fc1e6cd6575cfc21a2bf
Jason Cetina <jcetina@github.com>
2022-02-02 21:11:08 -0800

    policy and role assignment

--- remove_storage_settings.tf
+++ remove_storage_settings.tf
@@ -22,8 +22,7 @@ resource "azurerm_policy_definition" "modify-activity-log-settings" {
     "effect": "[parameters('effect')]",
     "details": {
       "roleDefinitionIds": [
-        "/providers/Microsoft.Authorization/roleDefinitions/749f88d5-cbae-40b8-bcfc-e573ddc772fa",
-        "/providers/Microsoft.Authorization/roleDefinitions/17d1049b-9a84-46fb-8f53-869881c3d3ab"
+        "/providers/Microsoft.Authorization/roleDefinitions/749f88d5-cbae-40b8-bcfc-e573ddc772fa"
       ],
       "operations": [
         {
@@ -59,4 +58,36 @@ POLICY_RULE
   }
 }
 PARAMETERS
+}
+
+resource "azurerm_policy_assignment" "modify-activity-log-settings" {
+  name                 = "modify-activity-logs"
+  location             = data.azurerm_resource_group.rg.location
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  policy_definition_id = azurerm_policy_definition.modify-activity-log-settings.id
+  description          = "modify-activity-log-settings"
+  display_name         = "modify-activity-log-settings"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+  parameters = <<PARAMETERS
+  {
+    "effect": {
+      "value": "Modify"
+    },
+    "profileName": {
+      "value": "setbypolicy_Diagnostics2Storage"
+    }
+  }
+PARAMETERS
+}
+
+resource "azurerm_role_assignment" "modify-activity-log-settings-role-assignment" {
+  principal_id         = azurerm_policy_assignment.modify-activity-log-settings.identity.0.principal_id
+  role_definition_name = "Monitoring Contributor"
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  description          = "modify-activity-log-settings"
 }
