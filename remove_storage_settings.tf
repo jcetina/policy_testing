c178260e66a1f196cc65848fe4526c8d89f2c49e
Jason Cetina <jcetina@github.com>
2022-02-04 14:25:14 -0800

    change name

--- remove_storage_settings.tf
+++ remove_storage_settings.tf
@@ -26,7 +26,7 @@ resource "azurerm_policy_definition" "modify-activity-log-settings" {
       ],
       "operations": [
         {
-          "field": "Microsoft.Insights/diagnosticSettings.name",
+          "field": "Microsoft.Insights/diagnosticSettings/name",
           "operation": "addOrReplace",
           "value": "disabled"
         },
