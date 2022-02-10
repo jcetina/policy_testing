b16932ad69c9b57ae7d159e9cb1d381b64890543
Jason Cetina <jcetina@github.com>
2022-02-10 09:38:29 -0800

    audit deployment

---
+++ deployment.tf
@@ -0,0 +1,132 @@
+
+
+resource "azurerm_policy_definition" "fix_activity_logs" {
+  name         = "fix-activity-logs"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Fix Activity Logs -> Storage"
+
+  policy_rule = <<POLICY_RULE
+{
+  "if": {
+    "allOf": [
+        {
+            "field": "Microsoft.Resources/deployments/parameters",
+            "containsKey": "storageAccountId"
+        },
+        {
+            "field": "Microsoft.Resources/deployments/parameters",
+            "containsKey": "storageAccountId2"
+        },
+        {
+            "field": "type",
+            "equals": "Microsoft.Resources/deployments"
+        }
+    ]
+  },
+  "then": {
+    "effect": "[parameters('effect')]"
+  }
+}
+POLICY_RULE
+
+  parameters = <<PARAMETERS
+{
+  "effect": {
+    "type": "String",
+    "metadata": {
+      "displayName": "Effect",
+      "description": "Enable or disable the execution of the policy"
+    },
+    "allowedValues": [
+      "DeployIfNotExists",
+      "AuditIfNotExists",
+      "Audit",
+      "Disabled"
+    ],
+    "defaultValue": "Audit"
+  },
+  "profileName": {
+    "type": "String",
+    "metadata": {
+      "displayName": "Profile name",
+      "description": "The diagnostic settings profile name"
+    },
+    "defaultValue": "setbypolicy_Diagnostics2Storage"
+  },
+  "storageAccountId": {
+    "type": "String",
+    "metadata": {
+      "displayName": "Storage Account resource ID",
+      "description": "Select Storage account from dropdown list. If this account is outside of the scope of the assignment you must manually grant 'Contributor' permissions (or similar) to the policy assignment's principal ID.",
+      "strongType": "Microsoft.Storage/storageAccounts"
+    }
+  },
+  "storageAccountId2": {
+    "type": "String",
+    "metadata": {
+      "displayName": "Storage Account resource ID",
+      "description": "Select Storage account from dropdown list. If this account is outside of the scope of the assignment you must manually grant 'Contributor' permissions (or similar) to the policy assignment's principal ID.",
+      "strongType": "Microsoft.Storage/storageAccounts"
+    }
+  }
+}
+PARAMETERS
+}
+
+
+resource "azurerm_policy_assignment" "fixactivitylogstostorage" {
+  name                 = "fix-activity-logs-to-storage"
+  location             = data.azurerm_resource_group.rg.location
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  policy_definition_id = azurerm_policy_definition.fix_activity_logs.id
+  description          = "Policy Assignment for Fixing Activity Logs -> Storage Account"
+  display_name         = "Fix Activity Logs -> Storage across tenant"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+  parameters = <<PARAMETERS
+  {
+    "effect": {
+      "value": "Audit"
+    },
+    "profileName": {
+      "value": "setbypolicy_Diagnostics2Storage"
+    },
+    "storageAccountId": {
+      "value": "/subscriptions/ad3b85d9-1354-4383-a30c-6383716082e4/resourceGroups/rg-gh-jcetina-policy-testing/providers/Microsoft.Storage/storageAccounts/jcetinapoltestast"
+    },
+    "storageAccountId2": {
+      "value": "/subscriptions/ad3b85d9-1354-4383-a30c-6383716082e4/resourceGroups/rg-gh-jcetina-policy-testing/providers/Microsoft.Storage/storageAccounts/jcetinapoltestbst"
+    }
+  }
+PARAMETERS
+}
+
+
+
+# resource "azurerm_role_assignment" "SecurityTelemetryRemediationStorageContributor" {
+#   principal_id         = azurerm_policy_assignment.activitylogstostorage.identity.0.principal_id
+#   role_definition_name = "Storage Account Contributor"
+#   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+#   description          = "terraform-managed: security_telemetry_remediation role Storage Account Contributor"
+# }
+
+# resource "azurerm_role_assignment" "SecurityTelemetryRemediationMonitorContributor" {
+#   principal_id         = azurerm_policy_assignment.activitylogstostorage.identity.0.principal_id
+#   role_definition_name = "Monitoring Contributor"
+#   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+#   description          = "terraform-managed: security_telemetry_remediation role Monitoring Contributor"
+# }
+
+
+resource "azurerm_policy_remediation" "remediateactivitylogs" {
+  name                    = "remediate-activity-logs"
+  scope                   = azurerm_policy_assignment.activitylogstostorage.scope
+  policy_assignment_id    = azurerm_policy_assignment.activitylogstostorage.id
+  resource_discovery_mode = "ExistingNonCompliant"
+}
+
